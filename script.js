d92fe0ea32767fb90023f0a7e39256bc397616b4
IcelandSpar <victorasielrodriguez@gmail.com>
2024-04-08 08:33:56 -0500

    Add clear board when button pressed for new grid
    Updated UI to reflect an etch-a-sketch toy. Board will clear now when grid is selected so it can be reloaded on page without having to refresh.

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
-function buildGrid(userCellChoice) {
-let widthAndLength = 800 / userCellChoice;
-console.log(widthAndLength);
+let userChoice = 4;
+
+let buildGrid = function (userCellChoice) {
+
+    let widthAndLength = 800 / userCellChoice;
+    console.log(widthAndLength);
 
 
 
+            for (i = 0; i < userCellChoice * userCellChoice; i++) {
+                
 
-
-        for (i = 0; i < userCellChoice * userCellChoice; i++) {
-            const gridContainer = document.querySelector('.gridContainer');
-            const cell = document.createElement('div');
-            gridContainer.classList.add('gridContainer');
-            cell.classList.add('cell');
-            gridContainer.appendChild(cell)
-            
-            cell.style.width = `${widthAndLength}px`;
-            cell.style.height = `${widthAndLength}px`;
-
-            cell.addEventListener('mouseover', () => {
-
-                cell.style.cssText = 'background-color: green;';
+                const gridContainer = document.querySelector('.gridContainer');
+                const cell = document.createElement('div');
+                gridContainer.classList.add('gridContainer');
+                cell.classList.add('cell');
+                gridContainer.appendChild(cell)
+                
                 cell.style.width = `${widthAndLength}px`;
                 cell.style.height = `${widthAndLength}px`;
-            });
 
-        };
+                cell.addEventListener('mouseover', () => {
+
+                    cell.style.cssText = 'background-color: green; border-color: rgba(0,0,0, 50%);';
+                    cell.style.width = `${widthAndLength}px`;
+                    cell.style.height = `${widthAndLength}px`;
+                    
+                    
+                });
+
+            };
+   
+
+};
 
 
 
@@ -32,22 +40,29 @@ console.log(widthAndLength);
 
 
 
-    } ;
-  
-    
+buildGrid(userChoice);
+
     
     const button = document.querySelector('.mysteriousButton');
     button.addEventListener('click', () => {
-        let userSquareNumber = prompt('How many squares per side would you like? \nThe max is 10.', '4')
-        if (userSquareNumber > 100 && userSquareNumber > 0) {
-            let userSquareNumber = prompt('Please choose a number less than 11 or bigger than 0.', '4');
-            console.log(userSquareNumber)
-            return userSquareNumber;
+        let userChoice = prompt('How many squares per side would you like? \nThe max is 100.', '4')
+        
+        if (userChoice > 100 || userChoice < 1) {
+            prompt('Please choose a number less than 101 or bigger than 0.', '4');
+            console.log(userChoice);
+            
+        } else if (userChoice < 101 && userChoice > 0) {
+            const gridContainer = document.querySelector('.gridContainer')
+            gridContainer.textContent = '';
+            
+            buildGrid(userChoice);
         } 
-    console.log(userSquareNumber)
+    
     });
     
     
     
-    buildGrid(5);
+
+ 
+    
     
